79edd920c983e2d8803debff7df6db5e9c89ca72
Michael Roose <mjroose@gmail.com>
2018-11-26 15:28:11 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'authors/:id/posts', to: 'authors#posts_index'
-  get 'authors/:id/posts/:post_id', to: 'authors#post'
 
-  resources :authors, only: [:show]
+  resources :authors, only: [:show] do
+    resources :posts, only: [:show, :index]
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
